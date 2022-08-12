2f8a07f838cea380221b89b32168951a9c1ec864
danielfajuyi <ayomidedaniels121@gmail.com>
2022-08-12 03:31:20 +0100

    without dark-mode

---
+++ portfolio_site_img/Portfolio website - dark mode/main.js
@@ -0,0 +1,12 @@
+var icon = document.getElementById("moonicon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    
+    if(document.body.classList.contains("dark-theme"))
+    {
+        icon.src = "./dark theme icon/sun.png";
+    } else{
+        icon.src = "./dark theme icon/moon.png";
+    }
+}
