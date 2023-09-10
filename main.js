0cd176fe1a84fcf00d8feeeba4b4a7c28c591b42
danielfajuyi <ayomidedaniels121@gmail.com>
2023-09-10 03:51:46 +0100

    new update

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 var icon = document.getElementById("moonicon");
 var span = document.getElementsByTagName("span");
-console.log(span)
 
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme"); 
-    
-    if(document.body.classList.contains("dark-theme"))
-    {
-        icon.src = "./dark theme icon/sun.png";
-    } else{
-        icon.src = "./dark theme icon/moon.png";
-    }
-}
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
 
-
-
-
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./dark theme icon/sun.png";
+  } else {
+    icon.src = "./dark theme icon/moon.png";
+  }
+};
